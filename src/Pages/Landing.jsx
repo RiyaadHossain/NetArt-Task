ba4bfc4bbf6ee1561d062cbe3e3cec7ca287a073
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-06-09 11:14:41 +0600

    file formatted

---
+++ src/Pages/Landing.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Banner from "../Components/Banner/Banner";
+import Brands from "../Components/Brands/Brands";
+import Footer from "../Components/Footer/Footer";
+import Header from "../Components/Header/Header";
+
+const Landing = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Brands />
+      <Footer />
+    </>
+  );
+};
+
+export default Landing;
